3ef28b25d77a98b35a26456008792af422105bfe
Maykonn <maykonn@outlook.com>
2019-01-07 13:43:21 -0200

    Clone function improved

--- install-rq.sh
+++ install-rq.sh
@@ -5,9 +5,9 @@ REPAIRQ_LOCAL_PROJECTS_DIR=${HOME}/Projects/repairq
 REPAIRQ_LOCAL_DOCKER_DIR_SUFFIX="/RepairQ-Docker"
 REPAIRQ_LOCAL_DOCKER_DIR=""
 
-echo Starting the installation
+echo $'Starting the installation\n'
 
 create_work_dir
 clone_docker_repo
 
-echo Installation finished
+echo $'\nInstallation finished'
--- lib.sh
+++ lib.sh
@@ -2,7 +2,7 @@
 
 create_work_dir() {
     # Change the LOCAL_REPAIRQ_PROJECTS_DIR var to the user given value or keep the default value if the given value is blank
-    read -r -p $'\nPath to your RepairQ local development directory.\nIf blank default will be assumed ['${REPAIRQ_LOCAL_PROJECTS_DIR}']: ' GIVEN;
+    read -r -p $'Path to your RepairQ local development directory.\nIf blank default will be assumed ['${REPAIRQ_LOCAL_PROJECTS_DIR}']: ' GIVEN;
     if [[ ! -z ${GIVEN} ]]; then
         REPAIRQ_LOCAL_PROJECTS_DIR=${GIVEN}
     fi
@@ -18,36 +18,25 @@ create_work_dir() {
 clone_docker_repo() {
     REPAIRQ_LOCAL_DOCKER_DIR=${REPAIRQ_LOCAL_PROJECTS_DIR}${REPAIRQ_LOCAL_DOCKER_DIR_SUFFIX}
 
-    create_the_project_dir() {
-        mkdir "$REPAIRQ_LOCAL_DOCKER_DIR"
-        echo $'\nDirectory created:'  ${REPAIRQ_LOCAL_DOCKER_DIR}
-    }
-
-    clone_the_project() {
-        echo "Cloning https://github.com/RepairQ/RepairQ-Docker.git into ${REPAIRQ_LOCAL_DOCKER_DIR}"
-        cd "$REPAIRQ_LOCAL_DOCKER_DIR" && git clone https://github.com/RepairQ/RepairQ-Docker.git ${REPAIRQ_LOCAL_DOCKER_DIR}
-    }
-
     echo $'\nCreating the RepairQ-Docker(https://github.com/RepairQ/RepairQ-Docker.git) local project.'
 
-    # The directory doesn't exists, will be created and the github project cloned into it
-    if [[ ! -d ${REPAIRQ_LOCAL_DOCKER_DIR} ]]; then
-        create_the_project_dir
-        clone_the_project
-
     # The directory already exists, asking the user how to proceed
-    else
+    if [[ -d ${REPAIRQ_LOCAL_DOCKER_DIR} ]]; then
         read -r -p $'Directory '${REPAIRQ_LOCAL_DOCKER_DIR}' already exists, do you want to overwrite it? [yes/cancel]: '
 
-        # Yes, or cancel the installation
-        [[ ${REPLY,,} =~ ^(c|cancel)$ ]] && { echo "Selected Cancel"; exit 1; }
+        # "y|yes|j|ja|s|si|o|oui", or cancel the installation
+        [[ ${REPLY,,} =~ ^(c|cancel|)$ ]] && { echo "Selected Cancel"; exit 1; }
         if [[ ${REPLY,,} =~ ^(y|yes|j|ja|s|si|o|oui)$ ]]; then
-            # Removes the directory, recreates and clone the project into it
             echo $'Removing the '${REPAIRQ_LOCAL_DOCKER_DIR}' dir'
             rm -rf ${REPAIRQ_LOCAL_DOCKER_DIR}
-
-            create_the_project_dir
-            clone_the_project
         fi
     fi
+
+    # If not cancelled by the user, continues to clone the repository into the correct directory:
+
+    mkdir "$REPAIRQ_LOCAL_DOCKER_DIR"
+    echo $'\nDirectory created:'  ${REPAIRQ_LOCAL_DOCKER_DIR}
+
+    echo "Cloning https://github.com/RepairQ/RepairQ-Docker.git into ${REPAIRQ_LOCAL_DOCKER_DIR}"
+    cd "$REPAIRQ_LOCAL_DOCKER_DIR" && git clone https://github.com/RepairQ/RepairQ-Docker.git ${REPAIRQ_LOCAL_DOCKER_DIR}
 }
